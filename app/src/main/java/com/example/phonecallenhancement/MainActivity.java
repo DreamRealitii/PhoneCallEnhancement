7c7afc1e2a03adb422a6d9c5e01900c62fc1d687
romerojhh <61641738+MrSaladdd@users.noreply.github.com>
2023-04-16 15:19:44 -0700

    init

--- app/src/main/java/com/example/phonecallenhancement/MainActivity.java
+++ app/src/main/java/com/example/phonecallenhancement/MainActivity.java
@@ -1,14 +1,176 @@
 package com.example.phonecallenhancement;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.res.ResourcesCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class MainActivity extends AppCompatActivity {
+
+    // CONSTANTS
+    public static int MY_PERMISSIONS_RECORD_AUDIO = 1;
+    public static int MY_PERMISSIONS_INTERNET = 2;
+
+    // UI COMPONENTS
+    private Button startService, pauseService, stopService;
+    private ActionBar actionBar;
+    private AudioRecord audioRecord;
+    private VisualizerView BeforeProcessWave, AfterProcessWave;
+
+    // FIELDS
+    private int bufferSize;
+    private BlockingQueue<short[]> audioDataQueue;
+    public short[] getAudioData() throws InterruptedException {
+        return audioDataQueue.take();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ExecutorService executorService = Executors.newFixedThreadPool(4);
+
+
+        startService = this.findViewById(R.id.btnStartService);
+        pauseService = this.findViewById(R.id.btnPauseService);
+        stopService = this.findViewById(R.id.btnStopService);
+        BeforeProcessWave = this.findViewById(R.id.beforeWave);
+        AfterProcessWave = this.findViewById(R.id.afterWave);
+
+        actionBar = getSupportActionBar();
+        actionBar.setTitle("Jancok");
+        actionBar.setBackgroundDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.bg, null));
+        actionBar.setHomeButtonEnabled(true);
+        actionBar.setDisplayOptions(ActionBar.DISPLAY_HOME_AS_UP);
+
+        setVolumeControlStream(AudioManager.STREAM_MUSIC);
+        checkRecordAudioPermission();
+        checkInternetPermission();
+        setupVisualizerFxAndUI();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (isFinishing() && audioRecord != null) {
+            stopRecording();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == MY_PERMISSIONS_RECORD_AUDIO) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission has been granted, proceed with audio capture
+                startRecording();
+            } else {
+                // Permission has been denied, show a message to the user or handle the error
+                Toast.makeText(this, "Audio capture permission denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    public void onButtonClick(View view) {
+        Button b = (Button) view;
+        Toast.makeText(this, "You clicked on " + b.getText(), Toast.LENGTH_SHORT).show();
+    }
+
+    private void checkRecordAudioPermission() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            // Permission has not been granted yet, request it from the user
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, MY_PERMISSIONS_RECORD_AUDIO);
+        } else {
+            // Permission has already been granted, proceed with audio capture
+            startRecording();
+        }
+    }
+
+    private void checkInternetPermission() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.INTERNET}, MY_PERMISSIONS_INTERNET);
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    public void startRecording() {
+        int sampleRate = 44100;
+        int channelConfig = AudioFormat.CHANNEL_IN_MONO;
+        int audioFormat = AudioFormat.ENCODING_PCM_16BIT;
+        bufferSize = AudioRecord.getMinBufferSize(sampleRate, channelConfig, audioFormat) ;
+
+        audioRecord = new AudioRecord(MediaRecorder.AudioSource.MIC, sampleRate, channelConfig, audioFormat, bufferSize);
+
+        audioDataQueue = new LinkedBlockingQueue<short[]>();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                short[] buffer = new short[bufferSize];
+                audioRecord.startRecording();
+                while (!Thread.currentThread().isInterrupted()) {
+                    int numBytes = audioRecord.read(buffer, 0, bufferSize);
+                    short[] data = new short[numBytes];
+                    System.arraycopy(buffer, 0, data, 0, numBytes);
+
+                    try {
+                        audioDataQueue.put(data);
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
+                }
+                audioRecord.stop();
+                audioRecord.release();
+            }
+        }).start();
+    }
+
+    public void stopRecording() {
+        audioRecord.stop();
+        audioRecord.release();
+    }
+
+
+
+    private void setupVisualizerFxAndUI() {
+        // Create a VisualizerView (defined below), which will render the simplified audio
+        // wave form to a Canvas.
+
+        Timer timer = new Timer();
+        long interval = 100; // 1/20 second in milliseconds
+
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                // This code will be executed every 1/20 second
+                try {
+                    BeforeProcessWave.updateVisualizer(getAudioData());
+                    AfterProcessWave.updateVisualizer(getAudioData());
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }, 0, interval);
+
     }
 }
---
+++ app/src/main/java/com/example/phonecallenhancement/SpeechToText.java
@@ -0,0 +1,134 @@
+package com.example.phonecallenhancement;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class SpeechToText extends AppCompatActivity implements RecognitionListener {
+    private SpeechRecognizer speechRecognizer;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.speech_text);
+
+        textView = findViewById(R.id.textView);
+
+        // Check if there are any speech recognition services installed
+        PackageManager packageManager = getPackageManager();
+        List<ResolveInfo> services = packageManager.queryIntentServices(
+                new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH), 0);
+
+        if (services.size() == 0) {
+            Log.d("Romero", "Speech recognition not supported on this device");
+        } else {
+            // Speech recognition supported, continue with initialization
+            Log.d("Romero", "Speech recognition supported");
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                speechRecognizer = SpeechRecognizer.createOnDeviceSpeechRecognizer(this);
+            } else {
+                speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+            }
+
+            speechRecognizer.setRecognitionListener(this);
+        }
+
+        String serviceComponent = Settings.Secure.getString(this.getContentResolver(), "voice_recognition_service");
+        ComponentName cn = ComponentName.unflattenFromString(serviceComponent);
+        System.out.println(cn.toShortString());
+//        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this, cn);
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            Log.d("Romero", "ondevice " + SpeechRecognizer.isOnDeviceRecognitionAvailable(this));
+        } else {
+            Log.d("Romero","recog " + SpeechRecognizer.isRecognitionAvailable(this));
+        }
+    }
+
+    public void startSpeechRecognition(View view) {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speak now");
+
+        try {
+            speechRecognizer.startListening(intent);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onReadyForSpeech(Bundle bundle) {
+        Log.d("Speech", "Ready for speech");
+    }
+
+    @Override
+    public void onBeginningOfSpeech() {
+        Log.d("Speech", "Beginning of speech");
+    }
+
+    @Override
+    public void onRmsChanged(float v) {
+        Log.d("Speech", "RMS changed");
+    }
+
+    @Override
+    public void onBufferReceived(byte[] bytes) {
+        Log.d("Speech", "Buffer received");
+    }
+
+    @Override
+    public void onEndOfSpeech() {
+        Log.d("Speech", "End of speech");
+    }
+
+    @Override
+    public void onError(int i) {
+        Log.d("Speech", "Error: " + i);
+    }
+
+    @Override
+    public void onResults(Bundle bundle) {
+        ArrayList<String> results = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+        if (results != null && !results.isEmpty()) {
+            String text = results.get(0);
+            textView.setText(text);
+        }
+    }
+
+    @Override
+    public void onPartialResults(Bundle bundle) {
+        Log.d("Speech", "Partial results");
+    }
+
+    @Override
+    public void onEvent(int i, Bundle bundle) {
+        Log.d("Speech", "Event: " + i);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        speechRecognizer.destroy();
+    }
+}
