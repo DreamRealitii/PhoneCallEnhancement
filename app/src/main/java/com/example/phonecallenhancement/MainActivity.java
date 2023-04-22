ea3902165ceaff16f1778b6f277bd45b51a5a0bd
Alan Yeung <yeungbluecat123@gmail.com>
2023-04-22 03:52:07 -0700

    a

--- app/src/main/java/com/example/phonecallenhancement/MainActivity.java
+++ app/src/main/java/com/example/phonecallenhancement/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.phonecallenhancement;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Environment;
+
+import com.aykuttasil.callrecord.CallRecord;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CallRecord callRecord = new CallRecord.Builder(this)
+                .setLogEnable(true)
+                .setRecordFileName("RecordFileName")
+                .setRecordDirName("RecordDirName")
+                .setRecordDirPath(Environment.getExternalStorageDirectory().getPath()) // optional & default value
+                .setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB) // optional & default value
+                .setOutputFormat(MediaRecorder.OutputFormat.AMR_NB) // optional & default value
+                .setAudioSource(MediaRecorder.AudioSource.VOICE_COMMUNICATION) // optional & default value
+                .setShowSeed(true) // optional & default value ->Ex: RecordFileName_incoming.amr || RecordFileName_outgoing.amr
+                .build();
+
+
+        callRecord.startCallReceiver();
     }
 }
